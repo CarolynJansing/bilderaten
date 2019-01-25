518d91e330588e618968183637bd6783b9ae98ba
CarolynJansing <46968716+CarolynJansing@users.noreply.github.com>
2019-01-25 10:20:57 +0100

    Add files via upload

---
+++ Bilderraten/win.php
@@ -0,0 +1,65 @@
+<style>
+
+
+.myButton {
+	-moz-box-shadow:inset 0px 1px 0px 0px #ffffff;
+	-webkit-box-shadow:inset 0px 1px 0px 0px #ffffff;
+	box-shadow:inset 0px 1px 0px 0px #ffffff;
+	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #ffffff), color-stop(1, #f6f6f6));
+	background:-moz-linear-gradient(top, #ffffff 5%, #f6f6f6 100%);
+	background:-webkit-linear-gradient(top, #ffffff 5%, #f6f6f6 100%);
+	background:-o-linear-gradient(top, #ffffff 5%, #f6f6f6 100%);
+	background:-ms-linear-gradient(top, #ffffff 5%, #f6f6f6 100%);
+	background:linear-gradient(to bottom, #ffffff 5%, #f6f6f6 100%);
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#ffffff', endColorstr='#f6f6f6',GradientType=0);
+	background-color:#ffffff;
+	-moz-border-radius:6px;
+	-webkit-border-radius:6px;
+	border-radius:6px;
+	border:1px solid #dcdcdc;
+	display:inline-block;
+	cursor:pointer;
+	color:#666666;
+	font-family:Arial;
+	font-size:44px;
+	font-weight:bold;
+	padding:6px 24px;
+	text-decoration:none;
+	text-shadow:0px 1px 0px #ffffff;
+	
+}
+
+
+body {
+background-image: url("wp.jpg");
+background-size: cover;}
+
+
+</style>
+<style type="text/css">
+
+.mycss
+{
+text-shadow:0px 0px 3px rgba(235,235,235,1);font-weight:normal;color:#000000;letter-spacing:1pt;word-spacing:3pt;font-size:32px;;font-family:comic sans, comic sans ms, cursive, verdana, arial, sans-serif;line-height:1.1;
+}
+
+</style>
+<style> .aufgedeckt { background-image: url("löwe.jpg");
+ background-attachment: fixed;
+  background-position: center; 
+  color: transparent;
+
+} 
+</style>
+<img class="myButton" src="win.jpg" alt="">
+
+<div class="tenor-gif-embed" data-postid="11332867" data-share-method="host" data-width="25%" data-aspect-ratio="1.0666666666666667"><a href="https://tenor.com/view/clapping-applause-bravo-clap-qoobee-gif-11332867">Clapping Applause GIF</a> from <a href="https://tenor.com/search/clapping-gifs">Clapping GIFs</a></div><script type="text/javascript" async src="https://tenor.com/embed.js"></script>
+
+ <p class="mycss">
+<?php echo "°°°°°°°°°°°°°°°°°°°<br>Nächtes Bild ? <br><br>°°°°°°°°°°°°°°°°°°°"; ?>
+</p>
+<br>
+
+<input class="myButton" type="button" value="Bild1" onclick="window.location.href='bildraten.php'" />
+<input class="myButton" type="button" value="Bild2" onclick="window.location.href='bildraten2.php'" />
+<input class="myButton" type="button" value="Bild3" onclick="window.location.href='bildraten3.php'" />
